8ac6186a1dee79fa103ac72f52f037e2c63c0943
Mamadou Saliou DIALLO <Mamadou-Saliou-DILLO>
2024-06-09 23:23:30 +0000

    first commit

---
+++ exo.js
@@ -0,0 +1,6 @@
+var salutation = "Bonjour ", nom = "saliou";
+var resultat = salutation + nom;
+alert(resultat);
+var nom = prompt("Entrez le nom");
+var prenom = prompt("Entrez le prenom");
+alert("Merci Mr. " + nom );
